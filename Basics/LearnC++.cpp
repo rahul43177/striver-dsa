b7acad7cd4c69a0efb45405559643771e3bf0a09
Rahul Mishra <rahul.mishra@impactanalytics.co>
2026-02-16 14:56:52 +0530

    Learning CPP

--- Basics/LearnC++.cpp
+++ Basics/LearnC++.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
-    
+    int n ; 
+    cin >> n ; 
+
+
+    while(n>0) {
+        cout << "The value is greater than 0 : " << n << endl;
+        n--; 
+    }
     return 0;
 }
